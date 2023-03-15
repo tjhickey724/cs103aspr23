e7a6ab811fad90886fac31442da7322067953374
tim <tjhickey@brandeis.edu>
2023-03-15 09:26:23 -0400

    added firstapp

---
+++ lesson21/test_todolist.py
@@ -0,0 +1,92 @@
+'''
+simple demo of fixtures for Python program with pytest
+use to test the TodoList class 
+'''
+
+import pytest
+import sqlite3
+from todolist3 import TodoList, to_dict, tuples_to_dicts
+
+@pytest.fixture
+def tuples():
+    " create some tuples to put in the database "
+    return [("test1", "run test0", 0), 
+            ("test2", "run test1", 1)
+           ]
+
+@pytest.fixture
+def returned_tuples(tuples):
+    " add a rowid to the beginning of each tuple "
+    return [(i+1,)+tuples[i] for i in range(len(tuples))]
+
+@pytest.fixture
+def returned_dicts(tuples):
+    " add a rowid to the beginning of each tuple "
+    return tuples_to_dicts([(i+1,)+tuples[i] for i in range(len(tuples))])
+
+@pytest.fixture
+def todo_path(tmp_path):
+    yield tmp_path / 'todo.db'
+
+@pytest.fixture(autouse=True)
+def todolist(todo_path,tuples):
+    "create and initialize the todo.db database in /tmp "
+    con= sqlite3.connect(todo_path)
+    cur = con.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS todo
+                    (title text, desc text, completed int)''')
+    for i in range(len(tuples)):
+        cur.execute('''insert into todo values(?,?,?)''',tuples[i])
+    # create the todolist database
+    con.commit()
+    td = TodoList(todo_path)
+    yield td
+    cur.execute('''drop table todo''')
+    con.commit()
+
+
+def test_selectAll(todolist,returned_dicts):
+    td = todolist
+    results = td.selectAll()
+    expected = returned_dicts
+    assert results == expected
+  
+def test_selectActive(todolist,returned_dicts):
+    td = todolist
+    results = td.selectActive()
+    expected = [d for d in returned_dicts if d['completed']==0]
+    assert results == expected
+
+def test_selectCompleted(todolist,returned_dicts):
+    td = todolist
+    results = td.selectCompleted()
+    expected = [d for d in returned_dicts if d['completed']==1]
+    assert results == expected
+
+def test_add(todolist,returned_dicts):
+    td = todolist
+    tuple = (len(returned_dicts)+1,'test','write add test',1)
+    todolist.add(to_dict(tuple))
+    results = td.selectAll()
+    assert results[-1] == to_dict(tuple)
+
+def test_delete(todolist,returned_dicts):
+    td = todolist
+    td.delete(1)
+    results = td.selectAll()
+    expected = returned_dicts
+    assert results == expected[1:]
+
+def test_set_complete(todolist,returned_dicts):
+    td = todolist
+    td.setComplete(1) 
+    results = td.selectActive()
+    assert results==[]
+
+
+
+
+    
+
+
+
---
+++ lesson21/todolist3.py
@@ -0,0 +1,69 @@
+'''
+TodoList is an Object Relational Mapping to the todo.db database
+
+The ORM will work map SQL rows with the schema
+    (rowid,title,desc,completed)
+to Python Dictionaries as follows:
+
+(5,'commute','drive to work',false) <-->
+{rowid:5,title:'commute',desc:'drive to work',completed:false)
+
+In place of SQL queries, we will have method calls.
+
+This app will store the data in a SQLite database ~/todo.db
+
+'''
+import sqlite3
+import os
+
+def to_dict(t):
+    ''' t is a tuple (rowid,title, desc,completed)'''
+    todo = {'rowid':t[0], 'title':t[1], 'desc':t[2], 'completed':t[3]}
+    return todo
+
+def tuples_to_dicts(ts):
+    return [to_dict(t) for t in ts]
+
+class TodoList():
+    def __init__(self,db_path):
+        self.db_path=db_path
+        self.runQuery('''CREATE TABLE IF NOT EXISTS todo
+                    (title text, desc text, completed int)''',())
+    
+    def selectActive(self):
+        ''' return all of the uncompleted tasks as a list of dicts.'''
+        return self.runQuery("SELECT rowid,* from todo where completed=0",())
+
+    def selectAll(self):
+        ''' return all of the tasks as a list of dicts.'''
+        return self.runQuery("SELECT rowid,* from todo",())
+
+    def selectCompleted(self):
+        ''' return all of the completed tasks as a list of dicts.'''
+        return self.runQuery("SELECT rowid,* from todo where completed=1",())
+
+    def add(self,item):
+        ''' create a todo item and add it to the todo table '''
+        return self.runQuery("INSERT INTO todo VALUES(?,?,?)",(item['title'],item['desc'],item['completed']))
+
+    def delete(self,rowid):
+        ''' delete a todo item '''
+        return self.runQuery("DELETE FROM todo WHERE rowid=(?)",(rowid,))
+
+    def setComplete(self,rowid):
+        ''' mark a todo item as completed '''
+        return self.runQuery("UPDATE todo SET completed=1 WHERE rowid=(?)",(rowid,))
+
+    def runQuery(self,query,tuple):
+        ''' return all of the uncompleted tasks as a list of dicts.'''
+        #con= sqlite3.connect(os.getenv('HOME')+'/todo.db')
+        con = sqlite3.connect(self.db_path)
+        cur = con.cursor() 
+        cur.execute(query,tuple)
+        tuples = cur.fetchall()
+        con.commit()
+        con.close()
+        return tuples_to_dicts(tuples)
+
+    
+        

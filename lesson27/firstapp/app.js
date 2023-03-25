8ae31d5cab6a71ffbe4367bf5fc67044cd89d0a8
tim <tjhickey@brandeis.edu>
2023-03-25 16:21:19 -0400

    added weather feature using US govt API

--- lesson27/firstapp/app.js
+++ lesson27/firstapp/app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 const layouts = require("express-ejs-layouts");
 const pw_auth_router = require('./routes/pwauth')
 const toDoRouter = require('./routes/todo');
+const weatherRouter = require('./routes/weather');
 
 const User = require('./models/User');
 
@@ -91,6 +92,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(pw_auth_router)
+
 app.use(layouts);
 
 app.get('/', (req,res,next) => {
@@ -105,6 +107,7 @@ app.get('/about',
 )
 
 app.use(toDoRouter);
+app.use(weatherRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- lesson27/firstapp/routes/weather.js
+++ lesson27/firstapp/routes/weather.js
@@ -1,5 +1,9 @@
 /*
   weather.js -- Router accessing APIs
+  This is a demo showing how to have an express server
+  get information by accessing multiple APIs.
+  In this case we use the US government weather API
+  which is a free service provided to the world.
 */
 const express = require('express');
 const router = express.Router();
@@ -10,10 +14,53 @@ router.get('/weather', (req,res,next) => {
     res.render('weather')
 })
 
+// returns a list of matches for an address
+// if there is a match the latlon is in the coordinates field
+const get_latlon = async (address) => {
+    address = encodeURI(address);
+    let url="https://geocoding.geo.census.gov/geocoder"+
+              "/locations/onelineaddress"+
+              "?address="+address+
+              "&benchmark=2020"+
+              "&format=json"
+    let response = await axios.get(url)
+    return response.data.result.addressMatches
+}
+
+// returns a forecast for any US location
+// given by lattitude and longitude
+const get_forecaseURL = async (latlon) => {
+  let url = "https://api.weather.gov/points/"+
+              latlon.y+","+latlon.x
+  const response = await axios.get(url)
+  return response.data.properties.forecast
+}
+
+const get_weather = async (address) => {
+  const matches = await get_latlon(address)
+  if (matches.length==0) {
+    return ([])
+  } else {
+    const url = await get_forecaseURL(matches[0].coordinates)
+    const response = await axios.get(url)
+    return (response.data.properties.periods)
+  }
+}
+
+router.post('/weather.json',
+  async (req,res,next) => {
+    const weather = await get_weather(req.body.address)
+    res.json(weather)
+  }
+)
+
 router.post('/weather',
   async (req,res,next) => {
-    res.json(req.body)
-  }
+    console.log('getting weather')
+    res.locals.address = req.body.address
+    res.locals.weather = await get_weather(req.body.address)
+    res.render('weatherForecast')
+}
+)
 
-  )
-
+  module.exports = router;

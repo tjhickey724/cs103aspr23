5be49af10cdc4db8ac790ee3fae66c2abffc02ab
tim <tjhickey@brandeis.edu>
2023-03-29 13:28:57 -0400

    added an edit feature to todolist

--- lesson28/firstapp/routes/todo.js
+++ lesson28/firstapp/routes/todo.js
@@ -24,13 +24,22 @@ isLoggedIn = (req,res,next) => {
 router.get('/todo/',
   isLoggedIn,
   async (req, res, next) => {
-      const completed = req.query.show=='completed'
-      res.locals.show = req.query.show
-      res.locals.items = 
-        await ToDoItem.find(
-           {userId:req.user._id, completed}).sort({completed:1,priority:1,createdAt:1})
-      res.render('toDoList');
+      const show = req.query.show
+      const completed = show=='completed'
+      let items=[]
+      if (show) { // show is completed or todo, so just show some items
+        items = 
+          await ToDoItem.find({userId:req.user._id, completed})
+                        .sort({completed:1,priority:1,createdAt:1})
+      }else {  // show is null, so show all of the items
+        items = 
+          await ToDoItem.find({userId:req.user._id})
+                        .sort({completed:1,priority:1,createdAt:1})
+
+      }
+            res.render('toDoList',{items,show,completed});
 });
+
 
 
 /* add the value in the body to the list associated to the key */
@@ -76,6 +85,28 @@ router.get('/todo/uncomplete/:itemId',
       res.redirect('/toDo')
 });
 
+router.get('/todo/edit/:itemId',
+  isLoggedIn,
+  async (req, res, next) => {
+      console.log("inside /todo/edit/:itemId")
+      const item = 
+       await ToDoItem.findById(req.params.itemId);
+      //res.render('edit', { item });
+      res.locals.item = item
+      res.render('edit')
+      //res.json(item)
+});
+
+router.post('/todo/updateTodoItem',
+  isLoggedIn,
+  async (req, res, next) => {
+      const {itemId,item,priority} = req.body;
+      console.log("inside /todo/complete/:itemId");
+      await ToDoItem.findOneAndUpdate(
+        {_id:itemId},
+        {$set: {item,priority}} );
+      res.redirect('/toDo')
+});
 
 
 module.exports = router;

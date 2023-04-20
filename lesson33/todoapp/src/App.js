523dfc805237c72849363c2e548f70bdc4010515
tim <tjhickey@brandeis.edu>
2023-04-20 13:21:37 -0400

    tried to add localstorate

--- lesson33/todoapp/src/App.js
+++ lesson33/todoapp/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const initTodos = [
   { key: 0, desc: "eat lunch", completed: false },
@@ -8,9 +8,22 @@ const initTodos = [
 ];
 
 export default function App() {
-  let [items, setItems] = useState(initTodos);
-  let [numKeys,setNumKeys] = useState(initTodos.length);
+  let [items, setItems] = useState(() =>getItems());
+  let [numKeys,setNumKeys] = useState(items.length);
 
+  useEffect( () => {
+    // update local storage each time items changes ...
+    const storeItems = async () => {
+      items = await localStorage.setItem('items',items)
+    };
+    storeItems();
+  },
+    [items]);
+
+  function getItems(){
+    let items = localStorage.getItem('items');
+    return(items || []);
+  }
   function add_item() {
     console.log("adding item");
     const item = document.getElementById("item").value;
@@ -24,11 +37,32 @@ export default function App() {
     setItems([...items, newItem]);
   }
 
+  function delete_item(itemKey) {
+    console.log('to be deleted:', itemKey);
+    const newItems = items.filter((item) => item.key !== itemKey);
+    setItems(newItems);
+  }
+
+  function toggle_completed(key) {
+    console.log("toggling completed");
+    let newItems = items.map((item) => {
+      if (item.key === key) {
+        item.completed = !item.completed;
+      }
+      return item;
+    });
+    setItems(newItems);
+  }
+
   return (
     <div className="App">
       <h1>Tim's Todo List</h1>
       {items.map((item) => (
-        <li>{item["desc"]}</li>
+        <li>
+           <button onClick={() => toggle_completed(item.key)}>{item.completed ? "c" : "+"}</button>
+           <button onClick={() => delete_item(item.key)}>X</button>
+          {item["desc"]}
+        </li>
       ))}
 
       <h2> new item </h2>

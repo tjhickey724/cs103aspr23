f81adda1bee1d1320606f5e6dce571d4171a796f
tim <tjhickey@brandeis.edu>
2023-04-20 13:34:21 -0400

    fixed localStorage bugs

--- lesson33/todoapp/src/App.js
+++ lesson33/todoapp/src/App.js
@@ -1,77 +1,102 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import "./styles.css";
-import { useState, useEffect } from "react";
+
+
 
 const initTodos = [
   { key: 0, desc: "eat lunch", completed: false },
   { key: 1, desc: "walk dog", completed: false }
 ];
 
-export default function App() {
-  let [items, setItems] = useState(() =>getItems());
-  let [numKeys,setNumKeys] = useState(items.length);
-
-  useEffect( () => {
-    // update local storage each time items changes ...
-    const storeItems = async () => {
-      items = await localStorage.setItem('items',items)
-    };
-    storeItems();
-  },
-    [items]);
-
-  function getItems(){
-    let items = localStorage.getItem('items');
-    return(items || []);
+function getItemsFromLocalStorage() {
+  // getting stored value
+  const saved = localStorage.getItem("items");
+  const initialValue = JSON.parse(saved)||[];
+  // relabel the keys from 0 to length-1
+  for(let i=0;i<initialValue.length;i++){
+    initialValue[i]['key']=i
   }
+  return initialValue || [];
+}
+
+
+export default function App() {
+
+  let [items, setItems] = useState(getItemsFromLocalStorage);
+  let [numKeys,setNumKeys] = useState(() => items.length)
+  let [msg,setMsg] = useState('none');
+
   function add_item() {
-    console.log("adding item");
+    // add an item to the todolist
     const item = document.getElementById("item").value;
-    let newItem = 
-    {
+    let newItem = {
       key: numKeys,
       desc: item,
       completed: false
     };
-    setNumKeys(numKeys+1);
-    setItems([...items, newItem]);
+    document.getElementById("item").value = ""
+    setNumKeys(numKeys+1)
+    setItems([newItem,...items]); // using the spread operator ...
   }
 
-  function delete_item(itemKey) {
-    console.log('to be deleted:', itemKey);
-    const newItems = items.filter((item) => item.key !== itemKey);
-    setItems(newItems);
+  function deleteItem(key){
+    console.log(key)
+    const newItems = items.filter((x)=> x['key']!== key)
+    setItems(newItems)
+    setNumKeys(numKeys-1)
   }
 
-  function toggle_completed(key) {
-    console.log("toggling completed");
-    let newItems = items.map((item) => {
-      if (item.key === key) {
-        item.completed = !item.completed;
-      }
-      return item;
-    });
-    setItems(newItems);
-  }
+  
 
+  useEffect(() => {
+    const getMsg = async () => {
+      const response = await fetch('http://localhost:3000/test');
+      const result = await response.json();
+      setMsg(result);
+      console.log('msg =',result);
+    }
+    getMsg()
+  },[msg])
+
+
+  useEffect(() => {
+    // storing items if items changes value
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      // 👇 Get input value
+      add_item();
+    }
+  };
   return (
     <div className="App">
       <h1>Tim's Todo List</h1>
-      {items.map((item) => (
-        <li>
-           <button onClick={() => toggle_completed(item.key)}>{item.completed ? "c" : "+"}</button>
-           <button onClick={() => delete_item(item.key)}>X</button>
-          {item["desc"]}
-        </li>
-      ))}
+      {msg}
+      <table>
+        <tbody>
+            {items.map((item) => (
+            <tr>
+              <td>{item["desc"]}</td>
+              <td><button onClick={()=>deleteItem(item["key"])}>X</button></td>
+            </tr>
+            ))}
+        </tbody>
+      </table>
 
-      <h2> new item </h2>
-      <input type="text" id="item" placeholder="description" />
+
+      <h2> add new todo item </h2>
+      <input type="text" 
+             onKeyDown={handleKeyDown}
+             id="item" placeholder="description" />
       <button onClick={() => add_item()}>add Todo</button>
-    
-    <h2>Items in JSON</h2>
-    <pre>{JSON.stringify(items,null,5)}</pre>
-
+      
+      <h2> list of items in JSON </h2>
+      <pre>
+       {JSON.stringify(items, null, 5)}
+      </pre>
     </div>
+
   );
 }
